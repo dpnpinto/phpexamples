43ce498d415e1964887e1d01f6463f98750a16f7
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-03-15 14:27:57 -0100

    Update exemplo_autov.php

--- exemplo_autov.php
+++ exemplo_autov.php
@@ -9,24 +9,29 @@
 // define variaveis e colocar todas com uma string a vazia 
 $name = $email = $gender = $comment = $website = "";
 
-function test_input($data) {
+function clean_input($data) {
+  //Retirar esaços em branco
   $data = trim($data);
+  //Retirar back slashes  "\"
   $data = stripslashes($data);
+  //REmover caracteres especiais
   $data = htmlspecialchars($data);
   return $data;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
+  $name = clean_input($_POST["name"]);
+  $email = clean_input($_POST["email"]);
+  $website = clean_input($_POST["website"]);
+  $comment = cleean_input($_POST["comment"]);
+  $gender = clean_input($_POST["gender"]);
 }
 
 ?>
 
-<h2>Exemplo de validação de formulário em PHP</h2>
+<h1>Exemplo de validação de formulário em PHP</h1>
+<h1>By https://dpnpinto.github.io </h1>
+!-- Esta ação permite que seja a propia página a receber o metodo, o htmlspecialchars permite a modificação do código e evitar exploits
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   O seu nomes: <input type="text" name="name">
   <br><br>
