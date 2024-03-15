631474dc7e25617906d194679f152430f9141aaf
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-03-15 12:23:16 -0100

    Update exemplo_autov.php

--- exemplo_autov.php
+++ exemplo_autov.php
@@ -6,8 +6,15 @@
 <body>  
 
 <?php
-// define variables and set to empty values
+// define variaveis e colocar todas com uma string a vazia 
 $name = $email = $gender = $comment = $website = "";
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
@@ -17,34 +24,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $gender = test_input($_POST["gender"]);
 }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
-<h2>PHP Form Validation Example</h2>
+<h2>Exemplo de validação de formulário em PHP</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name">
+  O seu nomes: <input type="text" name="name">
   <br><br>
-  E-mail: <input type="text" name="email">
+  O seu E-mail: <input type="text" name="email">
   <br><br>
-  Website: <input type="text" name="website">
+  O seu site: <input type="text" name="website">
   <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  Commentários: <textarea name="comment" rows="5" cols="40"></textarea>
   <br><br>
-  Gender:
-  <input type="radio" name="gender" value="female">Female
-  <input type="radio" name="gender" value="male">Male
-  <input type="radio" name="gender" value="other">Other
+  Qual o seu genero:
+  <input type="radio" name="gender" value="female">Femenino
+  <input type="radio" name="gender" value="male">Masculino
+  <input type="radio" name="gender" value="other">Outro
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
-echo "<h2>Your Input:</h2>";
+echo "<h1>O que foi inserido:</h1>";
 echo $name;
 echo "<br>";
 echo $email;
