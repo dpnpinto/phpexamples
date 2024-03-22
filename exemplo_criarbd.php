ff38006b14328229d12903a670afce315bfedaaf
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-03-22 16:38:09 -0100

    Update exemplo_criarbd.php

--- exemplo_criarbd.php
+++ exemplo_criarbd.php
@@ -1,1 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "pint20";
+
+// Criar a ligação à base de dados e colocar na variavel conn
+$conn = mysqli_connect($servername, $username, $password);
+// Se a variavel ficou sem um valor existiu erro na coneção
+if (!$conn) {
+  die("Coneção ao sgbd falhou: " . mysqli_connect_error());
+}
+
+// A criar uma base de dados
+$sql = "CREATE DATABASE myDB";
+// são passados na função a coneção e o SQL
+if (mysqli_query($conn, $sql)) {
+  echo "Foi criada a base de dados com sucesso";
+} else {
+  echo "Erro ao criar a base de dados: " . mysqli_error($conn);
+}
+//fechar a coneção à base de dados
+mysqli_close($conn);
+?>
 
