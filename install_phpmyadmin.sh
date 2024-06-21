4a3662ebb6fa19a1c42aa4ab2c8bbd22c5e159b1
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-06-21 14:57:20 +0000

    Update install_phpmyadmin.sh

--- install_phpmyadmin.sh
+++ install_phpmyadmin.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bs
+# Instalação do PHPMYADMIN para facil gestão de bases de dados para o projeto PHP
+# by https://dpnpinto.github.io
+sudo dnf -y install epel-release
+sudo dnf -y install phpmyadmin
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --add-service=https --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart httpd
+echo EDITAR o ficheiro phpMyAdmin.conf em /etc/httpd/conf.d/
+echo Colocar -> Require all granted no "/usr/share/phpMyAdmin/"
